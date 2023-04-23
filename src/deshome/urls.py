f8e01eb8a0f45439f3df63f1c10c6301f0c809db
D3stinn3 <paynedestinne@gmail.com>
2023-04-23 14:51:10 +0300

    Practical Changes

--- src/deshome/urls.py
+++ src/deshome/urls.py
@@ -20,7 +20,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name="home"),
+    path('', views.home_view, name='home'),
     path('admin/', admin.site.urls),
 ]
 
