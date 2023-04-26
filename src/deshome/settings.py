01578983f6b6cd9d2fa97a07812c2db013262b59
D3stinn3 <paynedestinne@gmail.com>
2023-04-26 17:33:44 +0300

    Settings on Base App

--- src/deshome/settings.py
+++ src/deshome/settings.py
@@ -33,6 +33,7 @@ AUTH_USER_MODEL = 'base.User'
 # Application definition
 
 INSTALLED_APPS = [
+    'base',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
