d43d9a66e010077e48a627eb74c2a77daca5a185
D3stinn3 <paynedestinne@gmail.com>
2023-05-12 14:11:12 +0300

    Admin

--- src/base/admin.py
+++ src/base/admin.py
@@ -5,7 +5,4 @@ from .models import User, Event, Submission
 admin.site.register(User)
 admin.site.register(Event)
 admin.site.register(Submission)
-<<<<<<< HEAD
-=======
 
->>>>>>> 3b3d3b382aff4fc0f01c50bcc88db5c983d6a460
