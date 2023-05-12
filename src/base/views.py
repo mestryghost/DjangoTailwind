0a12359a22ba0d97fd889fdc2b2dd6237375b7bc
D3stinn3 <paynedestinne@gmail.com>
2023-05-12 12:59:43 +0300

    Sync Continum

--- src/base/views.py
+++ src/base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import User, Event, Submission
 
 # Create your views here.
+
+def home_page(request):
+    users = User.objects.filter(party=True)
+    events = Event.objects.all()
+    context = {'users': users, 'event': events}
+    return render(request, 'home.html', context)
